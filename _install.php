1f277de1531f9f02cb86099948bedef516f420d6
Jean-Christian Denis <contact@jcdenis.fr>
2022-12-08 00:06:27 +0100

    no more need to set version

--- _install.php
+++ _install.php
@@ -114,11 +114,6 @@ try {
             true
         );
     }
-    # Set module version
-    dcCore::app()->setVersion(
-        $mod_id,
-        dcCore::app()->plugins->moduleInfo($mod_id, 'version')
-    );
 
     return true;
 } catch (Exception $e) {
